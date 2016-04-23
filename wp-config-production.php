8c7dca7ad8fd4e65a790f933a771e504f4085f53
Adam Runner <adamrunner@gmail.com>
2016-04-22 21:05:49 -0700

    updated wp-config for production

--- wp-config-production.php
+++ wp-config-production.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'doug6875_test');
+define('DB_NAME', 'doug6875_production');
 
 /** MySQL database username */
 define('DB_USER', 'doug6875_user');
